ca1b1d9923b67ad75df1036931708e49e7b415cb
Mohit Choudhary <mohitrj18.greybeard@gmail.com>
2026-04-18 22:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,23 @@ Author: Mohit RJ
 
 import os
 import sys
+import joblib
+
+base_dir = os.path.dirname(__file__)
+model_path = os.path.join(base_dir, "models", "model.pkl")
+
+model = joblib.load(model_path)
 
 # Fix encoding for cloud environments
 os.environ['PYTHONIOENCODING'] = 'utf-8'
 
 import streamlit as st
+
+@st.cache_resource
+def load_model():
+    return joblib.load("models/model.pkl")
+
+model = load_model()
 
 # PAGE CONFIG MUST be the FIRST Streamlit command (before any other imports
 # that might trigger Streamlit internally)
@@ -31,7 +43,6 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time
-import joblib
 
 # Add project root to path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
